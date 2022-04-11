9fd8b745591c5554076d761771c4b67ec07c6c79
Robinson <robinson@dorkbox.com>
2022-04-11 21:34:33 +0200

    Updated/added missing license information

--- src/dorkbox/netUtil/dnsUtils/ResolveConf.kt
+++ src/dorkbox/netUtil/dnsUtils/ResolveConf.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2016 dorkbox, llc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package dorkbox.netUtil.dnsUtils
 
 import com.sun.jna.Memory
@@ -9,8 +25,17 @@ import dorkbox.netUtil.IP
 import dorkbox.netUtil.jna.windows.IPHlpAPI
 import dorkbox.netUtil.jna.windows.structs.IP_ADAPTER_ADDRESSES_LH
 import dorkbox.netUtil.jna.windows.structs.IP_ADAPTER_DNS_SERVER_ADDRESS_XP
-import java.io.*
-import java.net.*
+import java.io.BufferedReader
+import java.io.FileReader
+import java.io.IOException
+import java.io.InputStream
+import java.io.InputStreamReader
+import java.net.Inet4Address
+import java.net.InetAddress
+import java.net.InetSocketAddress
+import java.net.URI
+import java.net.URISyntaxException
+import java.net.UnknownHostException
 import java.nio.file.Files
 import java.nio.file.Paths
 import java.util.*
