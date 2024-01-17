5785c9e72879da5b305f09c2c74f1eb82e2e579e
Robinson <robinson@dorkbox.com>
2024-01-17 12:30:51 +0100

    version 2.24

--- src/dorkbox/netUtil/Common.kt
+++ src/dorkbox/netUtil/Common.kt
@@ -30,7 +30,7 @@ internal object Common {
     /**
      * Gets the version number.
      */
-    const val version = "2.23"
+    const val version = "2.24"
 
     val OS_LINUX: Boolean
     val OS_WINDOWS: Boolean
